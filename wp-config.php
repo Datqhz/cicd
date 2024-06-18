d16408be5c8cbed56d980e20f902beee099a5cd4
Ho Quoc Dat <hodat2215@gmail.com>
2024-06-18 23:50:02 +0700

    .

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'cicd' );
+define( 'DB_NAME', 'test_db' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
@@ -48,14 +48,14 @@ define( 'DB_COLLATE', '' );
  *
  * @since 2.6.0
  */
-define( 'AUTH_KEY',         '`s%qw[2E^O6zQupsStKTv~yAIv6lijuF/lur;FxVj`p!  Na;k6SIxk>c`sZn%M)' );
-define( 'SECURE_AUTH_KEY',  'L([TX3WCOY 5;z!R;e[,)+TBsA2{Uf:PNGU,/>Dr)p6H rLE=,D8H)C=j*3u0 Cb' );
-define( 'LOGGED_IN_KEY',    '#u;|J/%on1)tN*Gv/)_xD32O6?GRbCixm]9*p74<3FXd!z3{av|zS=nV?64I$9K;' );
-define( 'NONCE_KEY',        ';j!n= RDb-Q90;3[pknN3,xPoNCHHXx_TQh`?/3ONiM`(?nd,[WE!78vF)f }k>p' );
-define( 'AUTH_SALT',        '|^mHr@p%}(owwfQj>i|T,=w3558gfGaaISc15unBt5OM)QyqG,rsSk02QZ-6X`;)' );
-define( 'SECURE_AUTH_SALT', '++/;>my$>usQk27P}Dk:Cc8q/00`-/fg7Bp4z>K> l64zCGbVBB9SjhA1wd:UrsP' );
-define( 'LOGGED_IN_SALT',   'rW-NK9-u}214H[>DsAESd~1biIb@?[bQXA6)i)T!32-> ogRO1NC^eC.tglfhK2 ' );
-define( 'NONCE_SALT',       'VB2xdbo(%4B<%6:zMpxT>qjn[H+j^~1S]G+@1R^[h+sjXe/oV a+}5ZIr*vc5^->' );
+define( 'AUTH_KEY',         'e1oQ_P/.78h)cw79cxxB:LT 3mg-`Z7W9R<6-eu|P1XQJOP+.L_%s!9q<(wMMpl-' );
+define( 'SECURE_AUTH_KEY',  '_TuZ-oHzP<tq;u&S7w22*1gt|]U![_5*Y%V+z90O%FjU=lpIylul/R/BOa3-Zf+0' );
+define( 'LOGGED_IN_KEY',    'ufzlV]fcyx YN7dvH~hko1a3E_R|o}&.k<UvKr(m}%_1$vf8/jy0Mb}}amzI;FN<' );
+define( 'NONCE_KEY',        'h*T}L7N6Rj`CZz14odGbLHYw=9dk(*kVPk4~AK6x4<8ZMTFIh#ICqgrG2?P-6$^e' );
+define( 'AUTH_SALT',        'MP>{i/nu|3swoBG:CxF*i{U_&z$ui/JokG?QJ&CHL/qm%BBb8 X0ASowaz;fRj[l' );
+define( 'SECURE_AUTH_SALT', '(5cp:0::LnKZ2~&LvtcmAj]!j++9R>z$%fbr?sMS/yr9pq]i!ngyM)UMcxw%r3^8' );
+define( 'LOGGED_IN_SALT',   'I[$WQ8pp7:f^YE33!U)q-<,?hEg^33{j-8I48M:yTl_T#~MV!{bF,rEe0Xt;md@H' );
+define( 'NONCE_SALT',       'TS5e$~gh<hf2tDDy6L_:(g^FFpf$r?_|%=bsTtG]Ko(721zc3Ncv][m1kuZ.J_;]' );
 
 /**#@-*/
 
